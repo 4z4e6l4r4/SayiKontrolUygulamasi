23dc9b7d2ff27b3d5d8ccc84aba3e850671415e0
Azra <yukselazrael@gmail.com>
2024-02-11 23:27:28 +0300

    Added Project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SayiKontrolUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            basadon:
+            Console.WriteLine("Sayı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 0)
+            {
+                Console.Clear();
+                goto basadon;
+            }
+            else
+            {
+                Console.WriteLine(sayi<0 ? "Sayı Negatif" : "Sayı Pozitif");
+            }
+        }
+    }
+}
